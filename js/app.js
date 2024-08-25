a4743c320ee7eeb0c3b22551669a12a6a079ee8b
Felipe Moreira <feliperioscross.br@gmail.com>
2024-08-25 03:06:30 -0300

    Responsividade, estilização

--- js/app.js
+++ js/app.js
@@ -1,33 +1,18 @@
-// Mapeamento das chaves de criptografia
-const encryptionKeys = {
-  'e': 'enter',
-  'i': 'imes',
-  'a': 'ai',
-  'o': 'ober',
-  'u': 'ufat'
-};
+const encryptionKeys = { 'e': 'enter', 'i': 'imes', 'a': 'ai', 'o': 'ober', 'u': 'ufat' };
+const decryptionKeys = Object.fromEntries(Object.entries(encryptionKeys).map(([key, value]) => [value, key]));
 
-// Inverso das chaves de criptografia para descriptografia
-const decryptionKeys = Object.fromEntries(
-  Object.entries(encryptionKeys).map(([key, value]) => [value, key])
-);
-
-// Função para criptografar texto
 function encrypt(text) {
   return text.replace(/[eioua]/g, match => encryptionKeys[match]);
 }
 
-// Função para descriptografar texto
 function decrypt(text) {
   return text.replace(/(enter|imes|ai|ober|ufat)/g, match => decryptionKeys[match]);
 }
 
-// Função para copiar texto para a área de transferência
 function copyToClipboard(text) {
-  navigator.clipboard.writeText(text)
+  navigator.clipboard.writeText(text);
 }
 
-// Manipuladores de eventos para os botões
 document.getElementById('encrypt-btn').addEventListener('click', () => {
   const inputText = document.getElementById('input-text').value;
   const encryptedText = encrypt(inputText);
